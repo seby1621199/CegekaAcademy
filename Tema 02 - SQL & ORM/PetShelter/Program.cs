c26c3140e0357cbf55e6a511af18be5493070edb
sebi <sebastian-ioan.paval@student.tuiasi.ro>
2023-02-27 18:57:47 +0200

    Projects

---
+++ Tema 02 - SQL & ORM/PetShelter/Program.cs
@@ -0,0 +1,22 @@
+
+using PetShelter.DataAccessLayer;
+using PetShelter.DataAccessLayer.Models;
+using PetShelter.DataAccessLayer.Repository;
+
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+PetShelterContext context = new PetShelterContext();
+DonationRepository DonationRepository = new DonationRepository(context);
+FundraiserRepository FundraiserRepository = new FundraiserRepository(context);
+app.MapGet("/", () =>
+{
+    //return DonationRepository.GetPeopleDonors();
+    return FundraiserRepository.GetTotalAmount(2); 
+});
+
+
+
+
+
+app.Run();
