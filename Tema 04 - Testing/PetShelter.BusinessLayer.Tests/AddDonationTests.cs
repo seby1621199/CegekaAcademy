38bde42a23794a21f2b937d67149aa0056093584
sebi <sebastian-ioan.paval@student.tuiasi.ro>
2023-03-03 22:37:45 +0200

    Tema 04 - Testing

---
+++ Tema 04 - Testing/PetShelter.BusinessLayer.Tests/AddDonationTests.cs
@@ -0,0 +1,125 @@
+﻿using Moq;
+using PetShelter.BusinessLayer.Validators;
+using PetShelter.DataAccessLayer.Models;
+using PetShelter.DataAccessLayer.Repository;
+
+namespace PetShelter.BusinessLayer.Tests;
+
+public class AddDonationTests
+{
+
+
+    [Fact]
+    public async Task GivenValidRequest_WhenAddDonation_DonationIsAdded()
+    {
+        var mockDonationRepository = new Mock<IDonationRepository>();
+        var donationServiceSut = new DonationService(mockDonationRepository.Object, new AddDonationRequestValidator());
+
+        var request = new AddDonationRequest
+        {
+            Amount = 10
+        };
+        await donationServiceSut.AddDonation(request);
+
+        mockDonationRepository.Verify(x => x.Add(It.Is<Donation>(d => d.Amount == request.Amount)), Times.Once);
+    }
+    //
+
+    [Fact]
+    public async Task GivenRequestWithMissingAmount_WhenAddDonation_DonationIsNotAdded()
+    {
+        var mockDonationRepository = new Mock<IDonationRepository>();
+        var donationServiceSut = new DonationService(mockDonationRepository.Object, new AddDonationRequestValidator());
+
+        var request = new AddDonationRequest();
+        await Assert.ThrowsAsync<ArgumentException>(() => donationServiceSut.AddDonation(request));
+
+        mockDonationRepository.Verify(x => x.Add(It.IsAny<Donation>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task GivenRequestWIthNegativeAmount_WhenAddDonation_DonationIsNotAdded()
+    {
+        var mockDonationRepository = new Mock<IDonationRepository>();
+        var donationServiceSut = new DonationService(mockDonationRepository.Object, new AddDonationRequestValidator());
+
+        var request = new AddDonationRequest
+        {
+            Amount = -700
+        };
+        await Assert.ThrowsAsync<ArgumentException>(() => donationServiceSut.AddDonation(request));
+
+        mockDonationRepository.Verify(x => x.Add(It.IsAny<Donation>()), Times.Never);   
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("a")]
+    public async Task GivenRequestWithWrongPersonName_WhenAddDonation_DonationIsNotAdded(string name)
+    {
+        var mockDonationRepository = new Mock<IDonationRepository>();
+        var donationServiceSut = new DonationService(mockDonationRepository.Object, new AddDonationRequestValidator());
+
+        var request = new AddDonationRequest
+        {
+            Amount = 700,
+            Person = new BusinessLayer.Models.Person
+            {
+                Name = name
+            }
+        };
+        await Assert.ThrowsAsync<ArgumentException>(() => donationServiceSut.AddDonation(request));
+
+        mockDonationRepository.Verify(x => x.Add(It.IsAny<Donation>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task GivenRequestWithWrongPersonIdNumber_WhenAddDonation_DonationIsNotAdded()
+    {
+        var mockDonationRepository = new Mock<IDonationRepository>();
+        var donationServiceSut = new DonationService(mockDonationRepository.Object, new AddDonationRequestValidator());
+
+        var request = new AddDonationRequest
+        {
+            Amount = 700,
+            Person = new BusinessLayer.Models.Person
+            {
+                IdNumber= "902906",
+                Name="salut"
+            }
+        };
+        await Assert.ThrowsAsync<ArgumentException>(() => donationServiceSut.AddDonation(request));
+
+        mockDonationRepository.Verify(x => x.Add(It.IsAny<Donation>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task GivenRequestWithWrongPersonAge_WhenAddDonation_DonationIsNotAdded()
+    {
+        var mockDonationRepository = new Mock<IDonationRepository>();
+        var donationServiceSut = new DonationService(mockDonationRepository.Object, new AddDonationRequestValidator());
+
+        var request = new AddDonationRequest
+        {
+            Amount = 700,
+            Person = new BusinessLayer.Models.Person
+            {
+                IdNumber = "1941129021906",
+                Name = "salutt",
+                DateOfBirth=DateTime.Now
+            }
+        };
+        await Assert.ThrowsAsync<ArgumentException>(() => donationServiceSut.AddDonation(request));
+
+        mockDonationRepository.Verify(x => x.Add(It.IsAny<Donation>()), Times.Never);
+
+    }
+
+
+
+
+
+
+
+
+}
---
+++ Tema 04 - Testing/PetShelter.BusinessLayer/Validators/AddDonationRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using PetShelter.BusinessLayer.Constants;
+using PetShelter.BusinessLayer.Tests;
+namespace PetShelter.BusinessLayer.Validators;
+
+public class AddDonationRequestValidator: AbstractValidator<AddDonationRequest>
+{
+	public AddDonationRequestValidator()
+	{
+        RuleFor(x => x.Amount).NotEmpty();
+        RuleFor(x => x.Amount).GreaterThan(0);
+        RuleFor(x => x.Person).NotEmpty()
+         .SetValidator(new PersonValidator());
+         RuleFor(x => x.Person.DateOfBirth).LessThanOrEqualTo(DateTime.Now.Date.AddYears(-PersonConstants.AdultMinAge))
+            .WithMessage("Adopter should be an adult.");
+        
+
+    }
+}
